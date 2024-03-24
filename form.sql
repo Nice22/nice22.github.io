3482005d30fd9b422c6eadd08913a4c0b3f73cf0
Nice22 <benothouessou74@gmail.com>
2024-03-24 17:08:37 +0100

    update_form

---
+++ form.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE FROM;
+USE myDatabase;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
